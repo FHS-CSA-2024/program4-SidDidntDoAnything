9bb1a0c028c3c3da9abef11ec5b952d9e4b120fb
SidDidntDoAnything <77165813+SidDidntDoAnything@users.noreply.github.com>
2024-08-13 15:19:33 -0700

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,10 +1,23 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
-
-
-
+public class Hello
+  {
+    public static void main(String[] args)
+    {
+      int n = 475;
+      int na = 821;
+      int nb = 369;
+      int nc = 562;
+      int sum = na + na + nb + nc;
+      double avg = sum/4.0;
+      System.out.println("The sum of the 4 numbers is " + sum + ".\n");
+      System.out.print("The average of the 4 numbers is " + avg + ".");
+    }
+  }
 //Paste console output below:
 /*
+The sum of the 4 numbers is 2573.
 
+The average of the 4 numbers is 643.25.
 
 */
